befd1d8a0746aded01cdd3b65bfa96bf02348208
Mark Rovetta <mkrnrt@gmail.com>
2018-04-09 21:19:17 -0700

    cleaned up code

--- chargaff.go
+++ chargaff.go
@@ -37,27 +37,24 @@ func main() {
 
 	fmt.Printf("%v bp\n", length)
 
-	fmt.Printf("A: %.1f%%\n", 100.0*(float64(counta)/float64(length)))
-	fmt.Printf("G: %.1f%%\n", 100.0*(float64(countg)/float64(length)))
-	fmt.Printf("C: %.1f%%\n", 100.0*(float64(countc)/float64(length)))
-	fmt.Printf("T: %.1f%%\n", 100.0*(float64(countt)/float64(length)))
-
-	fmt.Printf("A/T: %.2f\n", (float64(counta) / float64(countt)))
-	fmt.Printf("G/C: %.2f\n", (float64(countg) / float64(countc)))
-	
-	fmt.Printf("G+C: %.1f%%\n", 100.0*((float64(countg)+float64(countc))/float64(length)))	
-	fmt.Printf("A+T: %.1f%%\n", 100.0*((float64(counta)+float64(countt))/float64(length)))
-
 	a := 100.0*(float64(counta)/float64(length))
 	g := 100.0*(float64(countg)/float64(length))
 	c := 100.0*(float64(countc)/float64(length))
-	t := 100.0*(float64(countt)/float64(length))
+	t := 100.0*(float64(countt)/float64(length))	
 	
+	fmt.Printf("A: %.1f%%\n", a)
+	fmt.Printf("G: %.1f%%\n", g)
+	fmt.Printf("C: %.1f%%\n", c)
+	fmt.Printf("T: %.1f%%\n", t)
+	fmt.Printf("A/T: %.2f\n", a/t)
+	fmt.Printf("G/C: %.2f\n", g/c)	
+	fmt.Printf("G+C: %.1f%%\n", g+c)	
+	fmt.Printf("A+T: %.1f%%\n", a+t)
+
 	sa := strconv.FormatFloat(a, 'f', 2, 64)
 	sg := strconv.FormatFloat(g, 'f', 2, 64)
 	sc := strconv.FormatFloat(c, 'f', 2, 64)
 	st := strconv.FormatFloat(t, 'f', 2, 64)
-
 	sadt := strconv.FormatFloat((a/t), 'f', 2, 64)
 	sgdc := strconv.FormatFloat((g/c), 'f', 2, 64)
 	sgc := strconv.FormatFloat((g+c), 'f', 2, 64)
