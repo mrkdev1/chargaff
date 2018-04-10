765fbe130dad2cbf4e13393cf6eccd82c9f31d96
Mark Rovetta <mkrnrt@gmail.com>
2018-04-09 20:09:43 -0700

    read by lines fasta

--- chargaff.go
+++ chargaff.go
@@ -13,20 +13,27 @@ import (
 func main() {
     file := string(os.Args[1])
 	
-	
-	// Read file to byte slice
-    data, err := ioutil.ReadFile(file)
+    content, err := ioutil.ReadFile(file)
     if err != nil {
         log.Fatal(err)
     }
-	
-	str := string(data)
+    lines := strings.Split(string(content), "\n")
 
-	counta := strings.Count(str, "A")
-	countg := strings.Count(str, "G")
-	countc := strings.Count(str, "C")
-	countt := strings.Count(str, "T")
-	length := len(str)
+	fmt.Println(lines[0])
+	
+	counta := 0
+	countg := 0
+	countc := 0
+	countt := 0
+	length := 0
+
+	for i := 1; i < len(lines); i++ {
+	   counta = counta + strings.Count(lines[i], "A")
+	   countg = countg + strings.Count(lines[i], "G")
+	   countc = countc + strings.Count(lines[i], "C")
+	   countt = countt + strings.Count(lines[i], "T")
+	}
+	length = counta + countg + countc + countt
 
 	fmt.Printf("%v bp\n", length)
 
@@ -45,8 +52,6 @@ func main() {
 	g := 100.0*(float64(countg)/float64(length))
 	c := 100.0*(float64(countc)/float64(length))
 	t := 100.0*(float64(countt)/float64(length))
-			
-	fmt.Printf("Type: %T Value: %v\n", a, a)
 	
 	sa := strconv.FormatFloat(a, 'f', 2, 64)
 	sg := strconv.FormatFloat(g, 'f', 2, 64)
@@ -56,15 +61,17 @@ func main() {
 	sadt := strconv.FormatFloat((a/t), 'f', 2, 64)
 	sgdc := strconv.FormatFloat((g/c), 'f', 2, 64)
 	sgc := strconv.FormatFloat((g+c), 'f', 2, 64)
-	sat := strconv.FormatFloat((a+t), 'f', 2, 64)	
-	
-	fmt.Printf("Type: %T Value: %v\n", sa, sa)
+	sat := strconv.FormatFloat((a+t), 'f', 2, 64)
 
+	sdesc := lines[0]	
+	
+	title := ("# " + sdesc)
+    bpair := strconv.Itoa(length)	
 	head := ("| " + "A%" + " | " + "G%" +  " | " + "C%" +  " | " + "T%" +  " | " + "A/T" +  " | " + "G/C" +  " | " + "G+C" +  " | " + "A+T" +  " |")
 	bar := ("| " + " --- " + " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " | " + " --- " +  " |")
     row := 	("| " + sa + " | " + sg +  " | " + sc +  " | " + st +  " | " + sadt +  " | " + sgdc +  " | " + sgc +  " | " + sat +  " |")
 	
-    err = ioutil.WriteFile("output.md", []byte(head + "\n" + bar + "\n" + row + "\n"), 0666)
+    err = ioutil.WriteFile("output.md", []byte(title + "\n\n" + bpair + "bp" + "\n\n" + head + "\n" + bar + "\n" + row + "\n"), 0666)
     if err != nil {
         log.Fatal(err)
     }	
